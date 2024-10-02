7d683ae600f59876eea9ae13125984f5f6a5c90b
Vicente Duarte <vicentebcnduarte@gmail.com>
2024-10-02 23:22:33 +0100

    Ficheiros

---
+++ hva-app/src/hva/app/main/DoOpenFile.java
@@ -0,0 +1,24 @@
+package hva.app.main;
+
+import hva.HotelManager;
+import hva.app.exceptions.FileOpenFailedException;
+import hva.exceptions.UnavailableFileException;
+import pt.tecnico.uilib.forms.Form;
+import pt.tecnico.uilib.menus.Command;
+import pt.tecnico.uilib.menus.CommandException;
+//FIXME import other classes if needed
+
+class DoOpenFile extends Command<HotelManager> {
+    DoOpenFile(HotelManager receiver) {
+        super(Label.OPEN_FILE, receiver);
+    }
+
+    @Override
+    protected final void execute() throws CommandException {
+        /* try {
+            //FIXME implement command
+        } catch (UnavailableFileException e) {
+            throw new FileOpenFailedException(e);
+        } */
+    }
+}
---
+++ hva-core/src/hva/Hotel.java
@@ -0,0 +1,37 @@
+package hva;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import hva.exceptions.ImportFileException;
+import hva.exceptions.UnrecognizedEntryException;
+import java.io.IOException;
+
+//FIXME import other Java classes
+//FIXME import project classes
+
+public class Hotel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 202407081733L;
+
+    //FIXME define attributes
+    //FIXME define constructor(s)
+    //FIXME define methods
+
+    /**
+     * Read text input file and create domain entities.
+     *
+     * @param filename name of the text input file
+     * @throws ImportFileException
+     */
+    void importFile(String filename) throws ImportFileException {
+	    /*try {
+                // FIXME open import file and create the associated objects
+	        // ....
+            } catch (IOException | UnrecognizedEntryException e) {
+                throw new ImportFileException(filename, e);
+            }*/
+     }
+
+}
